5d561892173700c7d63aa9e8de540c41b46d7291
Jordhan Emmanuel <jordhanemmanuel@gmail.com>
2020-10-01 15:47:28 -0300

    Moveu de diretório

--- testes.js
+++
@@ -1,5 +0,0 @@
-let dataInfo = new Date()
-let dataHoje = dataInfo.getDate() + "/" + (dataInfo.getMonth()+1) + "/" + dataInfo.getFullYear()
-console.log(dataHoje)
-console.log(dataInfo)
-console.log(dataInfo.getMonth())
